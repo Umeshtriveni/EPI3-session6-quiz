ca1f1be4deba48b3cbb9b1a0769dfe50b4f33f3c
Umesh Birajdar <42119280+Umeshtriveni@users.noreply.github.com>
2021-06-25 19:16:07 +0530

    Update and rename Session6.py to session6.py

--- session6.py
+++ session6.py
@@ -1,55 +1,50 @@
-# part1
 import random
 from collections import Counter
+import math
+from functools import reduce
+from functools import partial
+import string
+import re
+import urllib
+#import requests
 
 value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
 suits = ['spades', 'clubs', 'hearts', 'diamonds']
 
 
-def genrate_deck(value,suits):
-  ''' This function takes suits and value list, combines them and returns the deck
+def generate_deck(value, suits):
+    ''' This function takes suits and value list, combines them and returns the deck
     # Input :
         suits: list
         value: list
     # Functionality:
         Iterates over suits and value and combines each value.
-    
-    # Returns: 
+
+    # Returns:
         A list is returned containing the result of combination of value and suits.
     '''
-  deck = []
-  for number in value:
-    for i in suits:
-      card = i + str(number)
-      deck.append(card)
-  return deck
+    value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
+    suits = ['spades', 'clubs', 'hearts', 'diamonds']
+    deck = [i+j for i in suits for j in value]
+    return deck
 
-test_deck = genrate_deck(value,suits)
-print(test_deck)
+generate_deck(value, suits)
 
 # Function generate deck of 52 cards Solely using map, lambda and zip function
-def generate_deck_using_lambda_map_zip(value,suits):
-  #using lambda zip and map fubction
-  x = list(map(lambda cards: ''.join(cards), zip( suits*len(value),value*len(suits))))
-  return x
+def generate_deck_using_lambda_map_zip(value, suits):
+    # using lambda zip and map fubction
+    value = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'jack', 'queen', 'king', 'ace']
+    suits = ['spades', 'clubs', 'hearts', 'diamonds']
+    x = list(map(lambda cards: ''.join(cards), zip(suits*len(value), value*len(suits))))
+    return x
 
-test_list = generate_deck_using_lambda_map_zip(value,suits)
+test_list = generate_deck_using_lambda_map_zip(value, suits)
 print(test_list)
-
-#Part 2
-import math
-from functools import reduce
-import random
-from functools import partial
-import string
-import re
-import urllib
-import requests
 
 # From a pre-stored fibonacci list, a given number has to be checked if it's a fibonacci number
 
 fib_list = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946]
-fib_check = lambda x : True if x in fib_list else False
+fib_check = lambda x: True if x in fib_list else False
 
 # From 2 lists, even number from 1st list and odd number from 2nd list should be added...using list comprehension, lambda/filter can also be used
 def even_odd(l1, l2):
@@ -58,7 +53,7 @@ def even_odd(l1, l2):
     if len(l2) == 0:
         raise ValueError('list2 cannot be empty')
     else:
-        return[x + y for x, y in zip(l1, l2) if x % 2 ==0 and y % 2 != 0]
+        return [x + y for x, y in zip(l1, l2) if x % 2 == 0 and y % 2 != 0]
 
 # Vowels should be stripped from a given string using list comprehension
 def strip_vowel_str(str):
@@ -92,7 +87,7 @@ def sigmoid_activation(l):
         else:
             pass
     else:
-        return[round(1/(1+math.exp(-x)),2) for x in l]
+        return[round(1/(1+math.exp(-x)), 2) for x in l]
 
 # function to shift the characters by 5 places
 def shift_5_char(str):
@@ -100,17 +95,18 @@ def shift_5_char(str):
         raise ValueError("string should have more than one character")
     else:
         str = str.lower()
-        return "".join([chr(ord(x)+5) if (ord(x)+5) <=122 else chr(ord(x)+5-122+96) for x in str])
+        return "".join([chr(ord(x)+5) if (ord(x)+5) <= 122 else chr(ord(x)+5-122+96) for x in str])
 
-response = requests.get("https://raw.githubusercontent.com/RobertJGabriel/Google-profanity-words/master/list.txt")
-data = response.text
-data
 
-def profane_filter(str):
-    if len(str) <= 1:
-        raise ValueError("string should have more than one character")
-    else:
-        return True if str.lower() in data else False
+# checks whether a string matches any of the swear words mentioned in https://github.com/RobertJGabriel/Google-profanity-words/blob/master/list.txt
+# response = requests.get("https://raw.githubusercontent.com/RobertJGabriel/Google-profanity-words/master/list.txt")
+# data = response.text
+# data
+# def profane_filter(str):
+#     if len(str) <= 1:
+#         raise ValueError("string should have more than one character")
+#     else:
+#         return True if str.lower() in data else False
 
 # Using reduce , lambda etc the even numbers in a list must be added.
 def add_even_num(l):
@@ -146,12 +142,11 @@ def add_third_num(l):
         return add_third_number
 
 # Using randint, random.choice and list comprehensions, 15 random KADDAADDDD number plates must be generated, where KA is fixed, D stands for a digit, and A stands for Capital alphabets
-def num_plate (x= 'KA', *y):
-    Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z']
-    return [x+ str(random.randint(10,99))+str(random.choice(Alphabet))+str(random.choice(Alphabet))+str(random.randint(1000,9999)) for i in range(15)]
+def num_plate(x='KA', *y):
+    Alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    return [x + str(random.randint(10,99)) + str(random.choice(Alphabet)) + str(random.choice(Alphabet)) + str(random.randint(1000, 9999)) for i in range(15)]
 
 # Using the above, we write a partial function such that 1000/9999 are hardcoded, but KA can be provided
-from functools import partial
-new_plate = partial(num_plate)
 
+new_plate = partial(num_plate)
 new_plate("DL")
